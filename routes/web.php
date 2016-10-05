38bda8f3fe9e49c1232b3a35d941b331742bc029
Erik Hallqvist <erikhallqvist@gmail.com>
2016-10-04 21:27:43 -0600

    Adding routes

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+  return view('index');
+});
+
+
+Route::get('/team/{id}', 'Team@display');
+
+Route::get('/player/{id}', function($id) {
+  return view('player', ['id' => $id]);
+});
+
+Route::get('/fa', function () {
+  return view('fa');
+});
+
+Route::get('/fa/{id}', function($id) {
+  return view('fa_offer', ['id' => $id]);
+});
+
